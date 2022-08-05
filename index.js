c771312b21162d3421cd4fc63f81b9de9a56a708
Cameron Matherne <cameronpmatherne@gmail.com>
2022-08-05 15:30:12 -0500

    small changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let gameBoard = (() => {
 
     let gameArray = [];
     let gb = document.querySelector('.gameBoard');
-
+ 
     // counter to keep track of current player
     // even number == player one
     // odd number == player two
@@ -33,7 +33,7 @@ let gameBoard = (() => {
     }
 
     function updateArray(id) {
-        if (gameArray[id] == " ") {
+        if (gameArray[id] == " ") { 
             if (counter % 2 == 0) {
                 gameArray[id] = "X";
                 counter++;
@@ -51,31 +51,38 @@ let gameBoard = (() => {
 
     function updateGrid(id) {
         let currentCell = document.getElementById(id);
-
         if (gameArray[id] == "X") {
             currentCell.innerText = "X";
         }
         if (gameArray[id] == "O") {
             currentCell.innerText = "O";
         }
-
-
-    }
-
-    function resetBoard() {
-        for (let i = 0; i < 9; i++) {
-            gameArray[i] == (" ");
+        if (gameArray[id] == " ") {
+            currentCell.innerText = " ";
         }
     }
 
+    function resetBoard() {
+
+        console.log("testing reset function");
+        gameArray = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+        for (let i = 0; i < 9; i++) {
+            updateGrid[i]
+        }
+        console.log(gameArray)
+    }
+
     return { generateBoard, resetBoard, scanForMove }
+
 })();
-
-
 
 
 gameBoard.generateBoard();
 gameBoard.scanForMove();
+
+let resetBtn = document.querySelector('.reset');
+resetBtn.addEventListener('click', gameBoard.resetBoard)
+
 
 
 
