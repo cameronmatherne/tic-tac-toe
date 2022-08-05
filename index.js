7fc2208936af247fd1a0b912953cf5076f33d811
Cameron Matherne <cameronpmatherne@gmail.com>
2022-08-04 19:16:31 -0500

    added board generation function inside gameboard module

--- index.js
+++ index.js
@@ -2,50 +2,56 @@
 
 
 // module 
-let gameBoard = (function () {
+let gameBoard = (() => {
 
     let gameArray = [];
     let gb = document.querySelector('.gameBoard');
 
+
     // create gameboard with CSS grid 
-    for (let i = 0; i < 9; i++) {
-        gameArray.push(" ");
-        let cell = document.createElement("div");
-        cell.innerText = "this is cell " + i;
-        cell.className = 'cell';
-        cell.id = 'cell' + i;
-        gb.appendChild(cell);
+    function generateBoard() {
+        for (let i = 0; i < 9; i++) {
+            gameArray.push(" ");
+            let cell = document.createElement("div");
+            cell.className = 'cell';
+            cell.id = 'cell' + i;
+            gb.appendChild(cell);
+        }
     }
 
+    function addEntry() {
 
 
+    }
+
+    function resetBoard() {
+        for (const i=0; i<9; i++) {
+            gameArray[i] == (" ");
+        }
+    }
+
+    return { generateBoard, addEntry, resetBoard }
 })();
-
-// factory for player creation 
-let playerFactory = (name) => {
-
-}
 
 let number = 0;
 
+
 let buttonOnGrid = document.querySelectorAll('div.cell');
 buttonOnGrid.forEach(cell => {
-
     cell.addEventListener('click', function () {
         number++;
         console.log('button clicked!');
 
-       if (number % 2 == 0) {
-        cell.innerText = "X"
-        cell.style.backgroundColor = 'red';
-       }
-       else {
-        cell.innerText = "O"
-        cell.style.backgroundColor = 'green';
-       }
-    
+        if (number % 2 == 0) {
+            cell.innerText = "X"
+            cell.style.backgroundColor = 'red';
+        }
+        else {
+            cell.innerText = "O"
+            cell.style.backgroundColor = 'green';
+        }
     });
 })
 
 
-
+gameBoard.generateBoard();
